ca6508ff396bff5e464e092ffd94a07ccd73ec6b
dano03001 <70076208+dano03001@users.noreply.github.com>
2020-11-06 09:42:24 +0100

    Add files via upload

---
+++ dcsaacs/dawdsa/dawdsa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dawdsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+
+            int rand_num = rd.Next(1,100);
+
+            Console.WriteLine("Gissa vilket tal mellan 1 och 100");
+            int tal= int.Parse(Console.ReadLine());
+
+            if (tal)
+            {
+
+            }
+        }
+    }
+}
